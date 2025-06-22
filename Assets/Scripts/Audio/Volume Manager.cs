3f8c1d889922135be26dc38896171b91bf438a99
spisebug <shloth03@gmail.com>
2025-06-22 10:21:53 -0500

    fixed a couple bugs
    fixed audio bug where settings wouldn't save after leaving the settings screen in the main menu and leaving. now, once the player releases the mouse button, audio settings will save.
    
    fixed a bug where the settings screen prefab was broken and not in the game scene. settings screen should be in it properly now.

--- Assets/Scripts/Audio/Volume Manager.cs
+++ Assets/Scripts/Audio/Volume Manager.cs
@@ -12,9 +12,8 @@ public class VolumeManager : MonoBehaviour
     [SerializeField] Slider musicSlider;
     [SerializeField] Slider soundSlider;
 
-    // TODO (6/16/2025)
-    // figure out why volume settings arent being saved
-    // once player exits setting screen menu and closes game
+    private float masterVolume, musicVolume, soundVolume;
+
     private void Start()
     {
         // if previous settings are found, we'll load the previous volume settings
@@ -24,25 +23,32 @@ public class VolumeManager : MonoBehaviour
     // master volume slider
     public void SetMasterVolume()
     {
-        float volume = masterVolumeSlider.value;
-        mixer.SetFloat("masterVolume", Mathf.Log10(volume) * 20);
-        GameData.Instance.masterVolume = volume;
+        masterVolume = masterVolumeSlider.value;
+        mixer.SetFloat("masterVolume", Mathf.Log10(masterVolume) * 20);
+        GameData.Instance.masterVolume = masterVolume;
     }
 
     // music volume slider
     public void SetMusicVolume()
     {
-        float volume = musicSlider.value;
-        mixer.SetFloat("music", Mathf.Log10(volume) * 20);
-        GameData.Instance.musicVolume = volume;
+        musicVolume = musicSlider.value;
+        mixer.SetFloat("music", Mathf.Log10(musicVolume) * 20);
+        GameData.Instance.musicVolume = musicVolume;
     }
 
     // sound volume slider
     public void SetSoundVolume()
     {
-        float volume = soundSlider.value;
-        mixer.SetFloat("sfx", Mathf.Log10(volume) * 20);
-        GameData.Instance.soundVolume = volume;
+        soundVolume = soundSlider.value;
+        mixer.SetFloat("sfx", Mathf.Log10(soundVolume) * 20);
+        GameData.Instance.soundVolume = soundVolume;
+    }
+
+    public void OnMouseUp()
+    {
+        GameData.Instance.masterVolume = masterVolume;
+        GameData.Instance.musicVolume = musicVolume;
+        GameData.Instance.soundVolume = soundVolume;
     }
 
     // loads volume settings
